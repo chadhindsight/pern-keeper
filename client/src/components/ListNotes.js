0d966f2b0cb1f223d9818a9a88a19920718b08b3
Chad Hinds <chadrickj8@gmail.com>
2021-08-10 11:58:48 -0400

    Add all components that will be used

---
+++ client/src/components/ListNotes.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+
+
+const ListNotes = () => {
+    // Get all notes from the db
+    const getNotes = () => {
+
+
+    }
+
+    useEffect(() => {
+        getNotes();
+    }, []);
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ListNotes;
