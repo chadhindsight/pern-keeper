f6572bc8125c07521e27e7b46185c07c1b7561f4
Chad Hinds <chadrickj8@gmail.com>
2021-08-10 15:03:34 -0400

    add a delete button the the list of notes

--- client/src/components/ListNotes.js
+++ client/src/components/ListNotes.js
@@ -2,9 +2,18 @@ import React, { useState, useEffect } from 'react';
 
 
 const ListNotes = () => {
-    // Get all notes from the db
-    const getNotes = () => {
+    const [notes, setNotes] = useState([]);
 
+    // Get all notes from the db
+    const getNotes = async () => {
+        try {
+            const response = await fetch("http://localhost:5000/notes");
+            const jsonData = await response.json();
+
+            setNotes(jsonData);
+        } catch (error) {
+            console.log(`${error} Sorry can't get your notes :(`)
+        }
 
     }
 
@@ -12,9 +21,33 @@ const ListNotes = () => {
         getNotes();
     }, []);
 
+    console.log(notes)
+
+    const deleteNote = async (id) => {
+        try {
+            const deleted = await fetch(`http://localhost:5000/${id}`, { method: "Delete" });
+            console.log(deleted)
+            // Return the updated list of notes
+            setNotes(notes.filter(note => note.note_id !== id));
+
+        } catch (error) {
+            console.log(`${error} Sorry that note was not deleted`)
+        }
+    }
+
     return (
         <div>
-
+            <ul>
+                {notes.map(noteMemo => {
+                    // Each one gets an EditNote model & Delete button
+                    <button
+                        className="btn btn-danger"
+                        onClick={() => deleteNote(noteMemo.note_id)}
+                    >
+                        -
+                    </button>
+                })}
+            </ul>
         </div>
     );
 };
