0fc9963c0cc9e3e94f23e8fac40c9d518c0ed3b5
Chad Hinds <chadrickj8@gmail.com>
2021-08-14 12:33:47 -0400

    Make a function called submitNote that lets users add new notes

---
+++ client/src/components/CreateNote.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+const CreateNote = () => {
+    const [newNote, setNewNote] = useState({
+        title: "",
+        memo: ""
+    })
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+
+        setNewNote((prevNote) => {
+            return {
+                // Keeps a hold of the old state data
+                ...prevNote,
+                [name]: value,
+            };
+        });
+    }
+
+    const submitNote = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('http://localhost:5000/notes', {
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(newNote)
+            })
+            console.log(response)
+
+        } catch (error) {
+            console.log(`${error} Sorry, could not submit your new note`)
+        }
+    }
+
+    return (
+        <div>
+            <form>
+                <input
+                    name="title"
+                    onChange={handleChange}
+                    value={newNote.title}
+                />
+                <textarea
+                    name="memo"
+                    onChange={handleChange}
+                    value={newNote.memo}
+
+                />
+                <button onClick={submitNote}>Add</button>
+            </form>
+        </div>
+    );
+};
+
+export default CreateNote;
