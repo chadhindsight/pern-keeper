41dfac9bbf7dd4b962686603bc7e9b018beeda52
Chad Hinds <chadrickj8@gmail.com>
2021-09-09 11:56:44 -0400

    Remove verbose comment

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.post("/notes", async (req, res) => {
 
 });
 
-// Read all notes. yeeerrr
+// Read all notes.
 app.get("/notes", async (req, res) => {
     try {
         const allNotes = await pool.query("SELECT * FROM note");
