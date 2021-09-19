be80d07905199edfe66bf31c1099a4fd385a4cf5
Chad Hinds <chadrickj8@gmail.com>
2021-09-19 09:40:33 -0400

    Add a header to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+// import './App.css';
 import './App.scss';
 import CreateNote from './components/CreateNote';
 import ListNotes from './components/ListNotes';
@@ -6,6 +6,7 @@ import ListNotes from './components/ListNotes';
 function App() {
   return (
     <div className="App">
+      <h1 id="#header">PERN KEEPER</h1>
       <CreateNote />
       <ListNotes />
     </div>
