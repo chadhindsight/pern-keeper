1065b8f1f398d6ea3296755e9bf3907b5f6cfed1
Chad Hinds <chadrickj8@gmail.com>
2021-11-01 09:04:12 -0400

    Fix comments

--- server/index.js
+++ server/index.js
@@ -66,7 +66,7 @@ app.put("/notes/:id", async (req, res) => {
 
 });
 
-// Delete a specific note note.
+// Delete a specific note note
 app.delete("/notes/:id", async (req, res) => {
     try {
         const { id } = req.params;
